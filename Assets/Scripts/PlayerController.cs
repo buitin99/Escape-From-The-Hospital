86f957c1ab1f20d882102b5231dce3dc7b368268
buitin99 <96711097+buitin99@users.noreply.github.com>
2022-09-19 14:59:05 +0700

    TinBQ up new input system 'Keyborad + Touch' & Floating JoyStick

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerController : MonoBehaviour
+{   
+    private CharacterController _characterController;
+    private PlayerInputAction _playerInputActions;
+    [SerializeField]
+    private float _speed = 0.1f;
+    private Vector2 _inputMove;
+
+    void FixedUpdate()
+    {
+        _inputMove = _playerInputActions.Player.Move.ReadValue<Vector2>();
+        _characterController.Move(new Vector3(_inputMove.x,0,_inputMove.y)*_speed);
+    }
+
+    private void Awake() {
+        _characterController = GetComponent<CharacterController>();
+        _playerInputActions = new PlayerInputAction();
+    }
+
+    private void OnEnable() {
+        _playerInputActions.Enable();   
+    }
+}
