25d73f88fc5caaf174d9081d4d264e5cc69531d9
buitin99 <96711097+buitin99@users.noreply.github.com>
2022-09-19 17:30:11 +0700

    up map

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,27 +1,41 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+    public class PlayerController : MonoBehaviour
+    {   
+        private CharacterController _characterController;
+        private PlayerInputAction _playerInputActions;
+        [SerializeField]
+        private float _speed = 1f;
+        private Vector2 _inputMove;
 
-public class PlayerController : MonoBehaviour
-{   
-    private CharacterController _characterController;
-    private PlayerInputAction _playerInputActions;
-    [SerializeField]
-    private float _speed = 1f;
-    private Vector2 _inputMove;
 
-    void FixedUpdate()
-    {
-        _inputMove = _playerInputActions.Player.Move.ReadValue<Vector2>();
-        _characterController.Move(new Vector3(_inputMove.x,0,_inputMove.y)*_speed);
+        private void Awake() {
+            _characterController = GetComponent<CharacterController>();
+            _playerInputActions = new PlayerInputAction(); 
+            
+            _playerInputActions.Player.Move.performed += SetDirMove;
+            _playerInputActions.Player.Move.canceled += SetDirMove;
+
+        }
+        private void OnEnable() {
+            _playerInputActions.Enable();   
+        }
+
+        void FixedUpdate()
+        {
+            _characterController.Move(new Vector3(_inputMove.x,0,_inputMove.y)*_speed);
+
+        }
+        
+        private void SetDirMove( InputAction.CallbackContext ctx) {
+            _inputMove = ctx.ReadValue<Vector2>();
+        }
+
+        private void OnDestroy() {
+            _playerInputActions.Player.Move.performed -= SetDirMove;
+            _playerInputActions.Player.Move.canceled -= SetDirMove;
+        }
+
     }
 
-    private void Awake() {
-        _characterController = GetComponent<CharacterController>();
-        _playerInputActions = new PlayerInputAction();
-    }
-
-    private void OnEnable() {
-        _playerInputActions.Enable();   
-    }
-}
