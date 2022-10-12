b642f995b0ec677395ef66081b701aa46ccf8077
buitin99 <96711097+buitin99@users.noreply.github.com>
2022-10-12 16:42:33 +0700

    update

--- Assets/Scripts/InvisiblePlayer.cs
+++ Assets/Scripts/InvisiblePlayer.cs
@@ -6,20 +6,23 @@ public class InvisiblePlayer : MonoBehaviour
 {
 
     private GameObject[] _bodyCharacters;
-    // private GameObject _shoes;
     public static bool isInvisible;
     // Start is called before the first frame update
     void Start()
     {
         _bodyCharacters = GameObject.FindGameObjectsWithTag("Invisible");
-        // _shoes = GameObject.FindGameObjectWithTag("Shoes");
-        // _shoes.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Potion"))
         {
-            other.GetComponent<PlayerPowerUp>().Invisible();
+            // other.GetComponent<PlayerPowerUp>().Invisible();
+            FindObjectOfType<PlayerPowerUp>().Invisible();
+
+            foreach(GameObject b in _bodyCharacters)
+            {
+                b.SetActive(false);
+            }
         }
     } 
 }
