f73470ad585f00a71f92c9f60f02d7f0077f8da8
buitin99 <96711097+buitin99@users.noreply.github.com>
2022-10-20 09:47:29 +0700

    ads

---
+++ Assets/Scripts/Ads/InterstitialAdExample.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Threading.Tasks;
+using Unity.Services.Core;
+using Unity.Services.Mediation;
+using UnityEngine;
+
+
+    public class InterstitialAdExample : MonoBehaviour
+    {
+        IInterstitialAd ad;
+        string adUnitId = "Interstitial_Android";
+        string gameId = "4981214";
+
+        private void Start() {
+            InitServices();
+        }
+
+        public async Task InitServices()
+        {
+            try
+            {
+                InitializationOptions initializationOptions = new InitializationOptions();
+                initializationOptions.SetGameId(gameId);
+                await UnityServices.InitializeAsync(initializationOptions);
+
+                InitializationComplete();
+            }
+            catch (Exception e)
+            {
+                InitializationFailed(e);
+            }
+        }
+
+        public void SetupAd()
+        {
+            //Create
+            ad = MediationService.Instance.CreateInterstitialAd(adUnitId);
+
+            //Subscribe to events
+            ad.OnClosed += AdClosed;
+            ad.OnClicked += AdClicked;
+            ad.OnLoaded += AdLoaded;
+            ad.OnFailedLoad += AdFailedLoad;
+            
+            // Impression Event
+            MediationService.Instance.ImpressionEventPublisher.OnImpression += ImpressionEvent;
+        }
+
+        public void Dispose() => ad?.Dispose();
+
+        
+        public async void ShowAd()
+        {
+            if (ad.AdState == AdState.Loaded)
+            {
+                try
+                {
+                    InterstitialAdShowOptions showOptions = new InterstitialAdShowOptions();
+                    showOptions.AutoReload = true;
+                    await ad.ShowAsync(showOptions);
+                    AdShown();
+                }
+                catch (ShowFailedException e)
+                {
+                    AdFailedShow(e);
+                }
+            }
+        }
+
+        void InitializationComplete()
+        {
+            SetupAd();
+            LoadAd();
+        }
+
+        async Task LoadAd()
+        {
+            try
+            {
+                await ad.LoadAsync();
+            }
+            catch (LoadFailedException)
+            {
+                // We will handle the failure in the OnFailedLoad callback
+            }
+        }
+
+        void InitializationFailed(Exception e)
+        {
+            Debug.Log("Initialization Failed: " + e.Message);
+        }
+
+        void AdLoaded(object sender, EventArgs e)
+        {
+            Debug.Log("Ad loaded");
+        }
+
+        void AdFailedLoad(object sender, LoadErrorEventArgs e)
+        {
+            Debug.Log("Failed to load ad");
+            Debug.Log(e.Message);
+        }
+        
+        void AdShown()
+        {
+            Debug.Log("Ad shown!");
+        }
+        
+        void AdClosed(object sender, EventArgs e)
+        {
+            Debug.Log("Ad has closed");
+            // Execute logic after an ad has been closed.
+        }
+
+        void AdClicked(object sender, EventArgs e)
+        {
+            Debug.Log("Ad has been clicked");
+            // Execute logic after an ad has been clicked.
+        }
+        
+        void AdFailedShow(ShowFailedException e)
+        {
+            Debug.Log(e.Message);
+        }
+
+        void ImpressionEvent(object sender, ImpressionEventArgs args)
+        {
+            var impressionData = args.ImpressionData != null ? JsonUtility.ToJson(args.ImpressionData, true) : "null";
+            Debug.Log("Impression event from ad unit id " + args.AdUnitId + " " + impressionData);
+        }
+        
+    }
---
+++ Assets/Scripts/Player/CMFreeLookPlayer.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CMFreeLookPlayer : MonoBehaviour
+{
+    //Used
+    public GameObject CMFL;
+}
